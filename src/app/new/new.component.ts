d205acff00bcd2f3c8e03e93e63adbba1ce1382a
Diego Suarez and Jim Padilla <diego+jim@>
2017-01-25 10:30:14 -0800

    add new crowdfund form

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  name: string;
+  email: string;
+  title: string;
+  description: string;
+  submitted: boolean;
+  type;
 
   constructor() { }
 
   ngOnInit() {
+    this.submitted = false;
+  }
+
+  submit(){
+    this.submitted = true;
+  }
+
+  newCrowdFund(){
+
+    console.log(this.name, this.email, this.title, this.description, this.type)
   }
 
 }
